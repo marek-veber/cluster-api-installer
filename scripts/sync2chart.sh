b65dfadca2e86cdd003501c21c28f16ffb363964
Marek Veber <mveber@redhat.com>
2025-01-07 12:21:52 +0100

    Update helm charts build

--- scripts/sync2chart.sh
+++ scripts/sync2chart.sh
@@ -16,20 +16,19 @@ fi
 
 
 CHARTDIR=../charts/$PROJECT
-PREVCHART=$BUILTDIR/prev-chart.yml
-NEWCHART=$BUILTDIR/new-chart.yml
+NEWCHART=../results/$PROJECT.yaml
 
 if [ "$SYNC2CHARTS" ] ;then
-    echo 'Run helm template before sync saving the output to ' $PREVCHART
-	helm template $CHARTDIR --include-crds > $PREVCHART
-	echo 'sync new output to ' $CHARTDIR
+    echo 'sync new output to ' $CHARTDIR
     rm -rf $CHARTDIR/templates/*.yaml
-	rm -rf $CHARTDIR/crds/*.yaml
+    rm -rf $CHARTDIR/crds/*.yaml
     mv $BUILTDIR/apiextensions*.yaml $CHARTDIR/crds
     mv $BUILTDIR/*.yaml $CHARTDIR/templates
-	sed -i -e 's/^\(version|appVersion\): .*/\1: "'"$OCP_VERSION"'"/' $CHARTDIR/Chart.yaml
-	sed -i -e 's/^\(    tag: vX.XX\).*/\1v'"$OCP_VERSION"/ "$CHARTDIR/values.yaml"
-	echo 'Run helm template after sync saving the output to ' $NEWCHART
-	helm template $CHARTDIR --include-crds > $NEWCHART
-	# TODO: We need to diff between PREVCHART with NEWCHART and write the difference in the logs
+    sed -i -e 's/^\(version\|appVersion\): .*/\1: "'"$OCP_VERSION"'"/' $CHARTDIR/Chart.yaml
+    sed -i -e 's/^\(    tag: \).*/\1v'"$OCP_VERSION"/ "$CHARTDIR/values.yaml"
+    echo 'Run helm template after sync saving the output to ' $NEWCHART
+
+    $HELM template $CHARTDIR --include-crds | \
+      $YQ ea '[.] | sort_by(.apiVersion,.kind,.metadata.name) | .[] | splitDoc|sort_keys(..)' | \
+      grep -v '^#' > $NEWCHART
 fi
