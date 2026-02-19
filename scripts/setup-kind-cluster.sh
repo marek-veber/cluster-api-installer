e9213580ab705a7879fd79aef0ce1e688b7652fc
Marek Veber <mveber@redhat.com>
2026-02-19 16:09:11 +0100

    fix

--- scripts/setup-kind-cluster.sh
+++ scripts/setup-kind-cluster.sh
@@ -15,7 +15,7 @@ HELM_INSTALL_TIMEOUT=${HELM_INSTALL_TIMEOUT:-10m}
 
 if ! (kind get clusters 2>/dev/null|grep -q '^'"$KIND_CLUSTER_NAME"'$') ; then
     SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-    if [ -z "$KIND_CFG_NAME"] ; then
+    if [ -z "$KIND_CFG_NAME" ] ; then
         if [ -n "$DOCKER_SECRETS" ] ; then
             KIND_CFG_NAME="$SCRIPT_DIR/kind-config-$KIND_CLUSTER_NAME.yaml"
 cat << EOF > "$KIND_CFG_NAME"
