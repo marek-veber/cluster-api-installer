d1d92c3822a850d528991a7aa93a8fad136b3954
Marek Veber <mveber@redhat.com>
2026-03-15 20:25:42 +0100

    fix

--- scripts/aro-hcp/gen.sh
+++ scripts/aro-hcp/gen.sh
@@ -41,7 +41,7 @@ if [ "$USE_CI" != "true" ] ; then
     fi
     
     if [ "$ENV" == stage ] ; then
-        export AZURE_SUBSCRIPTION_NAME=${AZURE_SUBSCRIPTION_NAME:-"ARO HCP - STAGE testing (EA Subscription)"}
+        export AZURE_SUBSCRIPTION_NAME=${AZURE_SUBSCRIPTION_NAME:-"ARO HCP - Misc Testing (EA Subscription)"}
         export REGION=${REGION:-uksouth}
     fi
 
